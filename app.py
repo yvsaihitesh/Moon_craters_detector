5ad18f246e9d17ab44510a54dc83a5b12020477e
yvsaihitesh <136172062+yvsaihitesh@users.noreply.github.com>
2025-06-19 09:52:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -40,5 +40,5 @@ def detect():
 
 if __name__ == '__main__':
     import os
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get('PORT', 7860))
     app.run(host='0.0.0.0', port=port)
